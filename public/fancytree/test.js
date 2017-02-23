48616362ed70033772000c53cc5c8cb3698a8aca
euqirneh <henrique60983@gmail.com>
2017-02-23 10:41:28 +0000

    .

--- public/fancytree/test.js
+++ public/fancytree/test.js
@@ -233,14 +233,14 @@ $(function() {
             $("#map-col").removeClass();
             $("#map-col").addClass("col-12");
             if (!$('#sidebar-show-btn').length) {
-                $("<button class='btn btn-inverse' type='button' data-placement='bottom' title='Mostrar painel' id='show_left_menu' onclick='show_left_menu();'><span class='fa fa-arrow-circle-o-right'></span></button>").insertBefore("#firstButton");
+                $("<button class='btn btn-inverse' type='button' data-placement='bottom' title='Mostrar painel' id='show_left_menu' onclick='show_left_menu();'><span class='fa fa-arrow-right'></span></button>").insertBefore("#firstButton");
             }
             map.updateSize();
         });
 
         $("#btnSortAll").click(function() {
             $("#tree").fancytree("getRootNode").sortChildren(null, true);
-        })
+        });
 
         window.show_left_menu = function() {
             $('#leftmenu-col').show();
@@ -250,37 +250,100 @@ $(function() {
             map.updateSize();
         };
 
+        //MODAL CONFIG 
         $("#btnInfo").click(function(e) {
             modalNodes = $("#tree").fancytree("getTree").getSelectedNodes();
-            console.log(modalNodes);
-            if (modalNodes.length > 0) {
+
+            var modalHeader = '<div id="infoModal" class="modal fade" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
+            modalHeader += '<div class="modal-dialog modal-lg">';
+            modalHeader += '<div class="modal-content">';
+            modalHeader += '<div class="modal-header">';
+            var modalTitle = '<h4 class="modal-title">' + modalNodes[0].title + '</h4><button type="button" class="close" data-dismiss="modal">×</button></div>';
+            var modalBody = '<div class="modal-body">'
+            modalBody += 'formContent</div>';
+            var modalFooter1 = '<div class="modal-footer">';
+            var modalPages1 = '<nav aria-label="Pages">';
+            modalPages1 += '<ul class="pagination justify-content-center" id="jaestouaficarfarto">';
+            modalPages1 += '<li class="page-item"><a class="page-link" href="#" tabindex="-1">Previous</a></li>';
+            var modalPages2 = '<li class="page-item"><a class="page-link" href="#">Next</a></li></ul></nav>';
+            var modalFooter2 = '<button class="btn btn-default" data-dismiss="modal">Close</button>';
+            var modalEnd = '</div></div></div></div>';
+
+            if (modalNodes.length == 1) {
                 console.log('titulo' + ($("#tree").fancytree("getTree").getSelectedNodes())[0].title)
-                html = '<div id="infoModal" class="modal fade" role="dialog" aria-labelledby="confirm-modal" aria-hidden="true">';
-                html += '<div class="modal-dialog modal-lg">';
-                html += '<div class="modal-content">';
-                html += '<div class="modal-header">';
-                html += '<h4 class="modal-title">' + modalNodes[0].title + '</h4>'
-                html += '<button type="button" class="close" data-dismiss="modal">×</button>';
-                html += '</div>';
-                html += '<div class="modal-body">';
-                html += 'formContent';
-                html += '</div>';
-                html += '<div class="modal-footer">';
-                html += '<button class="btn btn-default" data-dismiss="modal">Close</button>';
-                html += '</div>'; // content
-                html += '</div>'; // dialog
-                html += '</div>'; // footer
-                html += '</div>'; // modalWindow
+                var html = modalHeader + modalTitle + modalBody + modalFooter1 + modalFooter2 + modalEnd;
                 $('body').append(html);
                 modalNodes.clear;
                 $("#infoModal").modal();
                 $("#infoModal").on('hidden.bs.modal', function() {
-                    console.log('MALDITOS');
                     $(this).remove();
                 });
             }
+
+            if (modalNodes.length > 1) {
+                var modalTitles = [];
+                var itemNodes = [];
+                var modalNodes = $("#tree").fancytree("getTree").getSelectedNodes();
+                modalNodes.forEach(function(node) {
+                    if (!node.folder) {
+                        itemNodes.push(node);
+                    };
+                });
+                var createPages = "";
+                var pages = [];
+                for (var i = 1, x = itemNodes.length + 1; i < x; i++) {
+                    if (i == 1) {
+                        var page = '<li class="page-item"><a class="page-link"' + 'id="page' + i + '" href="#">' + i + '</a></li>';
+                        createPages += page;
+                        pages.push(page);
+                        modalTitles[i - 1] = '<h4 class="modal-title" id="modaltitle">' + itemNodes[i - 1].title + '</h4><button type="button" class="close" data-dismiss="modal">×</button></div>';
+                    } else if (i < 4) {
+                        var page = '<li class="page-item"><a class="page-link"' + 'id="page' + i + '" href="#">' + i + '</a></li>';
+                        createPages += page;
+                        pages.push(page);
+                        modalTitles[i - 1] = '<h4 class="modal-title">' + itemNodes[i - 1].title + '</h4><button type="button" class="close" data-dismiss="modal">×</button></div>';
+                    } else {
+                        break;
+                    }
+                };
+                var pagesHTML = [];
+                pages.forEach(function(page) {
+                    pagesHTML.push($.parseHTML(page));
+                });
+
+                var html = modalHeader + modalTitles[0] + modalBody + modalFooter1 + modalPages1 + modalPages2 + modalEnd;
+
+                $('body').append(html);
+                $('#jaestouaficarfarto').append(pagesHTML[0]);
+                $('#jaestouaficarfarto').append(pagesHTML[1]);
+                $('#jaestouaficarfarto').append(pagesHTML[2]);
+
+
+                function generateClickHandler(j) {
+                    return function(event) {
+                        document.getElementById('page' + j).parentNode.setAttribute('class', 'page-item');
+                        document.getElementById('modaltitle').innerText = itemNodes[j - 1].title;
+                        var centerPage = parseInt(document.getElementById('page' + j).parentNode.parentNode.children[2].children[0].text);
+                        var leftPage = centerPage + 1;
+                        var rightPage = centerPage + 2;
+                        document.getElementById('page' + leftPage).parentNode.parentNode.children[2].replaceWith(pagesHTML[0][0]);
+                        document.getElementById('page' + centerPage).parentNode.parentNode.children[3].replaceWith(pagesHTML[1][0]);
+                        document.getElementById('page' + rightPage).parentNode.parentNode.children[4].replaceWith(pagesHTML[2][0]);
+                    };
+                }
+
+                modalNodes.clear;
+                $("#infoModal").modal();
+
+                for (var i = 1, x = pagesHTML.length + 1; i < x; i++) {
+                    console.log(i);
+                    $('#page' + i).click(generateClickHandler(i));
+                }
+
+                $("#infoModal").on('hidden.bs.modal', function() {
+                    $(this).remove();
+                });
+            };
         });
-
-
     });
 });
