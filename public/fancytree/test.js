96c8d912255850475995e4e1002b99e82bd554e6
euqirneh <henrique60983@gmail.com>
2017-02-23 12:30:50 +0000

    é isso

--- public/fancytree/test.js
+++ public/fancytree/test.js
@@ -1,216 +1,216 @@
 $(function() {
     $(document).ready(function() {
-        glyph_opts = {
-            map: {
-                doc: "fa fa-file-o",
-                docOpen: "fa fa-file",
-                checkbox: "fa fa-square-o",
-                checkboxSelected: "fa fa-check-square",
-                checkboxUnknown: "fa fa-spinner",
-                error: "fa fa-exclamation-triangle",
-                expanderClosed: "fa  fa-arrow-right",
-                expanderLazy: "fa fa-arrow-right", // glyphicon-plus-sign
-                expanderOpen: "fa fa-arrow-down", // glyphicon-collapse-down
-                folder: "fa  fa-folder",
-                folderOpen: "fa fa-folder-open",
-                loading: "fa fa-spinner"
-            }
-        };
-        $("#tree").fancytree({
-            extensions: ["edit", "glyph", "wide"],
-            checkbox: true,
-            quicksearch: true,
-            glyph: glyph_opts,
-            clickFolderMode: 3,
-            selectMode: 3,
-            source: [ // Pass an array of nodes.
-                {
-                    title: "Item 1",
-                    children: [{
-                        title: "Sub-item 1.1",
-                        children: [
-                            { title: "Sub-item 1.1.1" }
-                        ]
-                    }]
-                },
-                {
-                    title: "Folder 2",
-                    folder: true,
-                    children: [
-                        { title: "Sub-item 2.1" },
-                        { title: "Sub-item 2.2" }
-                    ]
-                },
-                {
-                    title: "Item 3",
-                    children: [{
-                        title: "Sub-item 3.1",
-                        children: [
-                            { title: "Sub-item 3.1.1" }
-                        ]
-                    }]
-                },
-                {
-                    title: "Folder 4",
-                    folder: true,
-                    expanded: true,
-                    children: [{
-                            title: "Folder 4.1",
-                            folder: true,
-                            children: [{
-                                title: "Folder 4.1.1",
-                                folder: true,
-                                expanded: true,
-                                children: [{
-                                        title: "Item 4.1.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.1.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.1.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.1.1.4"
-                                    }
-                                ]
-                            }]
-                        },
-                        {
-                            title: "Folder 4.2",
-                            folder: true,
-                            children: [{
-                                title: "Folder 4.2.1",
-                                folder: true,
-                                children: [{
-                                        title: "Item 4.2.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.2.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.2.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.2.1.4"
-                                    }
-                                ]
-                            }]
-                        },
-                        {
-                            title: "Folder 4.3",
-                            folder: true,
-                            expanded: true,
-                            children: [{
-                                title: "Folder 4.3.1",
-                                folder: true,
-                                expanded: true,
-                                children: [{
-                                        title: "Item 4.3.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.3.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.3.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.3.1.4"
-                                    }
-                                ]
-                            }]
-                        },
-                        {
-                            title: "Folder 4.4",
-                            folder: true,
-                            children: [{
-                                title: "Folder 4.4.1",
-                                folder: true,
-                                children: [{
-                                        title: "Item 4.4.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.4.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.4.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.4.1.4"
-                                    }
-                                ]
-                            }]
-                        },
-                        {
-                            title: "Folder 4.5",
-                            folder: true,
-                            children: [{
-                                title: "Folder 4.5.1",
-                                folder: true,
-                                children: [{
-                                        title: "Item 4.5.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.5.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.5.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.5.1.4"
-                                    }
-                                ]
-                            }]
-                        },
-                        {
-                            title: "Folder 4.6",
-                            folder: true,
-                            children: [{
-                                title: "Folder 4.6.1",
-                                folder: true,
-                                children: [{
-                                        title: "Item 4.6.1.1"
-                                    },
-                                    {
-                                        title: "Item 4.6.1.2"
-                                    },
-                                    {
-                                        title: "Item 4.6.1.3"
-                                    },
-                                    {
-                                        title: "Item 4.6.1.4"
-                                    }
-                                ]
-                            }]
-                        }
-                    ]
-                }
-            ],
-            toggleEffect: { effect: "drop", options: { direction: "left" }, duration: 400 },
-            wide: {
-                iconWidth: "1em", // Adjust this if @fancy-icon-width != "16px"
-                iconSpacing: "0.5em", // Adjust this if @fancy-icon-spacing != "3px"
-                levelOfs: "1.5em" // Adjust this if ul padding != "16px"
-            },
-            select: function(event, data) {
-                // Display list of selected nodes
-                var selNodes = data.tree.getSelectedNodes();
-                console.log(selNodes);
-                // convert to title/key array
-                var selKeys = $.map(selNodes, function(node) {
-                    return "[" + node.key + "]: '" + node.title + "'";
-                });
-                $("#echo").text(selKeys.join(", "));
-            },
-            click: function(event, data) {
-                // We should not toggle, if target was "checkbox", because this
-                // would result in double-toggle (i.e. no toggle)
-                console.log(data);
-                if ($.ui.fancytree.getEventTargetType(event) === "title") {
-                    data.node.toggleSelected();
-                }
-            },
-        });
+        /* glyph_opts = {
+             map: {
+                 doc: "fa fa-file-o",
+                 docOpen: "fa fa-file",
+                 checkbox: "fa fa-square-o",
+                 checkboxSelected: "fa fa-check-square",
+                 checkboxUnknown: "fa fa-spinner",
+                 error: "fa fa-exclamation-triangle",
+                 expanderClosed: "fa  fa-arrow-right",
+                 expanderLazy: "fa fa-arrow-right", // glyphicon-plus-sign
+                 expanderOpen: "fa fa-arrow-down", // glyphicon-collapse-down
+                 folder: "fa  fa-folder",
+                 folderOpen: "fa fa-folder-open",
+                 loading: "fa fa-spinner"
+             }
+         };
+         $("#tree").fancytree({
+             extensions: ["edit", "glyph", "wide"],
+             checkbox: true,
+             quicksearch: true,
+             glyph: glyph_opts,
+             clickFolderMode: 3,
+             selectMode: 3,
+             source: [ // Pass an array of nodes.
+                 {
+                     title: "Item 1",
+                     children: [{
+                         title: "Sub-item 1.1",
+                         children: [
+                             { title: "Sub-item 1.1.1" }
+                         ]
+                     }]
+                 },
+                 {
+                     title: "Folder 2",
+                     folder: true,
+                     children: [
+                         { title: "Sub-item 2.1" },
+                         { title: "Sub-item 2.2" }
+                     ]
+                 },
+                 {
+                     title: "Item 3",
+                     children: [{
+                         title: "Sub-item 3.1",
+                         children: [
+                             { title: "Sub-item 3.1.1" }
+                         ]
+                     }]
+                 },
+                 {
+                     title: "Folder 4",
+                     folder: true,
+                     expanded: true,
+                     children: [{
+                             title: "Folder 4.1",
+                             folder: true,
+                             children: [{
+                                 title: "Folder 4.1.1",
+                                 folder: true,
+                                 expanded: true,
+                                 children: [{
+                                         title: "Item 4.1.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.1.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.1.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.1.1.4"
+                                     }
+                                 ]
+                             }]
+                         },
+                         {
+                             title: "Folder 4.2",
+                             folder: true,
+                             children: [{
+                                 title: "Folder 4.2.1",
+                                 folder: true,
+                                 children: [{
+                                         title: "Item 4.2.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.2.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.2.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.2.1.4"
+                                     }
+                                 ]
+                             }]
+                         },
+                         {
+                             title: "Folder 4.3",
+                             folder: true,
+                             expanded: true,
+                             children: [{
+                                 title: "Folder 4.3.1",
+                                 folder: true,
+                                 expanded: true,
+                                 children: [{
+                                         title: "Item 4.3.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.3.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.3.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.3.1.4"
+                                     }
+                                 ]
+                             }]
+                         },
+                         {
+                             title: "Folder 4.4",
+                             folder: true,
+                             children: [{
+                                 title: "Folder 4.4.1",
+                                 folder: true,
+                                 children: [{
+                                         title: "Item 4.4.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.4.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.4.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.4.1.4"
+                                     }
+                                 ]
+                             }]
+                         },
+                         {
+                             title: "Folder 4.5",
+                             folder: true,
+                             children: [{
+                                 title: "Folder 4.5.1",
+                                 folder: true,
+                                 children: [{
+                                         title: "Item 4.5.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.5.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.5.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.5.1.4"
+                                     }
+                                 ]
+                             }]
+                         },
+                         {
+                             title: "Folder 4.6",
+                             folder: true,
+                             children: [{
+                                 title: "Folder 4.6.1",
+                                 folder: true,
+                                 children: [{
+                                         title: "Item 4.6.1.1"
+                                     },
+                                     {
+                                         title: "Item 4.6.1.2"
+                                     },
+                                     {
+                                         title: "Item 4.6.1.3"
+                                     },
+                                     {
+                                         title: "Item 4.6.1.4"
+                                     }
+                                 ]
+                             }]
+                         }
+                     ]
+                 }
+             ],
+             toggleEffect: { effect: "drop", options: { direction: "left" }, duration: 400 },
+             wide: {
+                 iconWidth: "1em", // Adjust this if @fancy-icon-width != "16px"
+                 iconSpacing: "0.5em", // Adjust this if @fancy-icon-spacing != "3px"
+                 levelOfs: "1.5em" // Adjust this if ul padding != "16px"
+             },
+             select: function(event, data) {
+                 // Display list of selected nodes
+                 var selNodes = data.tree.getSelectedNodes();
+                 console.log(selNodes);
+                 // convert to title/key array
+                 var selKeys = $.map(selNodes, function(node) {
+                     return "[" + node.key + "]: '" + node.title + "'";
+                 });
+                 $("#echo").text(selKeys.join(", "));
+             },
+             click: function(event, data) {
+                 // We should not toggle, if target was "checkbox", because this
+                 // would result in double-toggle (i.e. no toggle)
+                 console.log(data);
+                 if ($.ui.fancytree.getEventTargetType(event) === "title") {
+                     data.node.toggleSelected();
+                 }
+             },
+         });*/
 
         $("#btnExpandAll").click(function() {
             $("#tree").fancytree("getTree").visit(function(node) {
@@ -317,7 +317,6 @@ $(function() {
                 $('#jaestouaficarfarto').append(pagesHTML[0]);
                 $('#jaestouaficarfarto').append(pagesHTML[1]);
                 $('#jaestouaficarfarto').append(pagesHTML[2]);
-
 
                 function generateClickHandler(j) {
                     return function(event) {
