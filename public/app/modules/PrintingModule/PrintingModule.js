a7d7576a5e6200b83943697146b771fa94231e3b
JoKord <jtpcordeiro@gmail.com>
2017-03-16 15:26:14 +0000

    Finished Second Page

--- public/app/modules/PrintingModule/PrintingModule.js
+++ public/app/modules/PrintingModule/PrintingModule.js
@@ -3,17 +3,40 @@
 
     angular
         .module('PrintingModule')
-        .controller('PagesController', PagesController);
+        .controller('PagesController', PagesController)
+        .controller('FormController', FormController);
 
     function PagesController() {
         var pagesCtrl = this;
         activate();
         this.active = 2;
-        this.activeTab = function(tab){
+        this.activeTab = function (tab) {
             return tab == pagesCtrl.active;
+        }
+        this.nextPage = function(valid){
+            if(valid){
+                pagesCtrl.active++;
+            }
         }
         function activate() {
             pagesCtrl.ctrlName = "PagesController";
+        }
+    }
+
+    function FormController() {
+        var formCtrl = this;
+        activate();
+
+        this.userData = {
+            requerente: '',
+            proprietario: '',
+            nif: null,
+            freguesia: '',
+            local: ''
+        }
+
+        function activate() {
+            formCtrl.ctrlName = "FormController";
         }
     }
 })();
