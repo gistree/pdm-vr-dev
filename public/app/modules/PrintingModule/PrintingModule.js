dfbdb3e85bf13c7452225161e7a31751ba361b6d
JoKord <jtpcordeiro@gmail.com>
2017-03-17 14:05:33 +0000

    Testing The Server CORS

--- public/app/modules/PrintingModule/PrintingModule.js
+++ public/app/modules/PrintingModule/PrintingModule.js
@@ -50,12 +50,12 @@
         this.details = {};
     }
 
-    LayoutSelectionController.$inject = ['PrintDetailsService'];
+    LayoutSelectionController.$inject = ['PrintDetailsService', '$http'];
 
-    function LayoutSelectionController(PrintDetailsService) {
+    function LayoutSelectionController(PrintDetailsService, $http) {
         var layoutCtrl = this;
         activate();
-        
+
         layoutCtrl.change = function () {
             layoutCtrl.noSelect = !layoutCtrl.layouts.some(function (layout) {
                 return layout.selected === true;
@@ -83,8 +83,64 @@
             //TODO
             // 2. Get the data for request
             // First We Test
-            
+            var printSpec = {
+                layout: "pdmLayout",
+                srs: "EPSG:3857",
+                units: "m",
+                outputFilename: "PDM_VilaReal",
+                outputFormat: "pdf",
+                mapTitle: "Plano Director Municipal - Vila Real",
+                layers: [{
+                    type: "WMS",
+                    format: "image/png",
+                    layers: ["PDM-VilaReal-database:RESERVA_ECOLOGICA_NACIONAL"],
+                    baseURL: "http://gistree.espigueiro.pt/geoserver/wms",
+                    customParams: {
+
+                    }
+                }],
+                pages: [{
+                    mapTitle: "Título do Mapa",
+                    center: [-862594.0274085791, 5055714.580579155],
+                    scale: 10000,
+                    dpi: 300,
+                    requerente: "João Cordeiro",
+                    nif: "123456789",
+                    proprietario: "Gistree",
+                    freguesia: "Folhadela",
+                    local: "Folhadela"
+                }]
+            };
+
             // 3. Make the request
+            // GET REQUEST WORKS
+            /*$http({
+                method: 'GET',
+                url: 'http://gistree.espigueiro.pt/geoserver/pdf/print.pdf',
+                params: {
+                    "spec": printSpec
+                }
+            }).then(function successCallback(response) {
+                console.log("Response");
+                console.log(response);
+            }, function errorCallback(response) {
+                console.log("Error");
+                console.log(response);
+            });*/
+            // POST REQUEST
+            $http.post("http://gistree.espigueiro.pt/geoserver/pdf/create.json",
+                printSpec, {
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                }
+            ).then(function successCallback(response) {
+                console.log("Response");
+                console.log(response);
+            }, function errorCallback(response) {
+                console.log("Error");
+                console.log(response);
+            });
             // 4. Print the Results
         }
     }
