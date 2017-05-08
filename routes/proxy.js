751d668aa687057039306a0c45075e473d1ed426
JoKord <jtpcordeiro@gmail.com>
2017-05-08 10:12:41 +0100

    changed proxy settings

--- routes/proxy.js
+++ routes/proxy.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // Proxy Server Definition
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var geoserver = 'http://gistree.espigueiro.pt/geoserver/wms/';
+var geoserver = 'http://localhost/geoserver/wms/';
 
 apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
     proxyReq.setHeader('X-Credentials', 'user=' + res.locals.username + '&pw=' + res.locals.pw);
