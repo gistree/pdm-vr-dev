ae1aafea9b6dd414af0378f4c7c623fb57781782
JoKord <jtpcordeiro@gmail.com>
2017-05-12 17:00:51 +0100

    new Teste

--- routes/printing.js
+++ routes/printing.js
@@ -6,13 +6,17 @@ var fs = require('fs');
 // Proxy Server Definition
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var geoserverPrint = 'http://localhost:8081/';
+var geoserverPrint = 'http://localhost:8081/geoserver/pdf/create.json';
 
+apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
+    console.log(req.baseURL);
+    proxyReq.setHeader('X-Credentials', 'user=' + res.locals.username + '&pw=' + res.locals.pw);
+});
 router.post('/', function (req, res) {
     console.log("REACHED HERE");
-    apiProxy.web(req, res, {
+    /*apiProxy.web(req, res, {
         target: geoserverPrint
-    });
+    });*/
 });
 
 module.exports = router;
