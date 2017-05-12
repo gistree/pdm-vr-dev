882fb4820b8cd2c2eadc1576a872152d1fe9c3f0
JoKord <jtpcordeiro@gmail.com>
2017-05-12 16:40:23 +0100

    Started Printing Module

---
+++ routes/printing.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var fs = require('fs');
+
+// Proxy Server Definition
+var httpProxy = require('http-proxy');
+var apiProxy = httpProxy.createProxyServer();
+var geoserverPrint = 'http://localhost:8081/geoserver/pdf/create.json';
+
+router.get('/', function (req, res) {
+    apiProxy.web(req, res, {
+        target: geoserverPrint
+    });
+});
+
+module.exports = router;
