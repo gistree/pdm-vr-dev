5b52b22ee379c27bfc1aa8bdb3cdb97f7f56da33
JoKord <jtpcordeiro@gmail.com>
2017-05-15 10:13:31 +0100

    Stream Body

--- routes/printing.js
+++ routes/printing.js
@@ -9,7 +9,14 @@ var apiProxy = httpProxy.createProxyServer();
 var geoserverPrint = 'http://localhost:8081/geoserver/pdf/create.json';
 
 apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
-    console.log(req.body);
+    if (req.body) {
+        let bodyData = JSON.stringify(req.body);
+        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxyReq.write(bodyData);
+    }
 });
 
 router.post('/', function (req, res) {
