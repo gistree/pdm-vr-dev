87d5d36501c6217bc9a70b821f7b7320b478d35a
JoKord <jtpcordeiro@gmail.com>
2017-05-15 10:53:18 +0100

    new 1.6

--- routes/printing.js
+++ routes/printing.js
@@ -9,20 +9,14 @@ var apiProxy = httpProxy.createProxyServer();
 var geoserverPrint = 'http://localhost:8081/geoserver/pdf/';
 
 apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
-    console.log(proxyReq.path);
     proxyReq.path = proxyReq.path + "create.json";
-    console.log(proxyReq.path);
 });
 
 apiProxy.on('error', function (err, req, res) {
-    console.log("IM IN ERROR");
     console.log(err);
 });
 
 router.post('/', function (req, res) {
-    console.log(req.path);
-    req.path = req.path + "create.json";
-    console.log(req.path);
     apiProxy.web(req, res, {
         target: geoserverPrint,
         ignorePath: true
