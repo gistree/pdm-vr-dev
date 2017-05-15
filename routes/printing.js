91110428ceae4bb275413da021f1d0df793b3926
JoKord <jtpcordeiro@gmail.com>
2017-05-15 12:12:26 +0100

    new 2.2

--- routes/printing.js
+++ routes/printing.js
@@ -13,20 +13,23 @@ apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
         proxyReq.path = proxyReq.path + "create.json";
         let bodyData = JSON.stringify(req.body);
         // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-        proxyReq.setHeader('Content-Type','application/json');
+        proxyReq.setHeader('Content-Type', 'application/json');
         proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
         // stream the content
         proxyReq.write(bodyData);
     }
 });
 
-apiProxy.on('proxyRes', function(proxyRes, req, res){
-    console.log("::::PROXY RES::::");
-    console.log(proxyRes);
-    console.log("::::SERVER RESPONSE::::");
-    console.log(res);
-    console.log("::::RES BODY::::");
-    console.log(res.body);
+apiProxy.on('proxyRes', function (proxyRes, req, res) {
+    res.on("data", (msg) => {
+        console.log("msg", msg)
+    })
+    res.on("end", () => {
+        console.log("end")
+    })
+    res.on("finish", (data) => {
+        console.log("finish", data)
+    })
 });
 
 apiProxy.on('error', function (err, req, res) {
