8587dcb705a781b6ae18f2a89a86494ffb544baa
JoKord <jtpcordeiro@gmail.com>
2017-05-15 10:55:47 +0100

    changed URL

--- routes/printing.js
+++ routes/printing.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 // Proxy Server Definition
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var geoserverPrint = 'http://localhost:8081/geoserver/pdf/';
+var geoserverPrint = 'http://gistree.espigueiro.pt:8081/geoserver/pdf/';
 
 apiProxy.on('proxyReq', function (proxyReq, req, res, options) {
     proxyReq.path = proxyReq.path + "create.json";
